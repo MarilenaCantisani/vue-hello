eaa8433f1d5994da48dd2a0c7ec321bb4bbb10c7
MarilenaCantisani <marilena.cantisani97@gmail.com>
2021-07-27 17:09:40 +0200

    + Added img with "v-bind" and added function that generates card at the click of the button

--- js/script.js
+++ js/script.js
@@ -1,9 +1,7 @@
-
-console.log("Test: ", Vue);
-
-// Link to activate vue in the inspector
+// Link to activate Vue in the inspector
 Vue.config.devtools = true;
 
+// Variable start Vue
 const root = new Vue({
     el: "#root",
     data: {
@@ -16,5 +14,11 @@ const root = new Vue({
         address: "",
         profession: "",
         image: "./user-img.png",
+        visibility: "d-none",
     },
-})
+    methods: {
+        generateCard() {
+            this.visibility = "d-block"
+        }
+    }
+});
